0b3988d58a36d2e0c9e2d9651a825eff6fb443a6
eliudroma <eliudromangupe@gmail.com>
2024-04-18 18:11:59 -0600

    etiquetado_componentes_1.2

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from umbralizacion_otsu import umbralizar_otsu_var_clases as otsu
 from imagenes import obtener_nombres_imagenes_aleatorias as nombres_img
 from imagenes import invertir as inv_img
 
+from EtiquetadoComponentes import primer_iteracion_componentes_etiquetados_conecta8 as primer_etiquetado_conecta8
+from EtiquetadoComponentes import primer_iteracion_componentes_etiquetados_conecta4 as primer_etiquetado_conecta4
+
+from EtiquetadoComponentes import EtiquetadoComponentes_conecta8 as segundo_etiquetado_conecta8
+from EtiquetadoComponentes import EtiquetadoComponentes_conecta4 as segundo_etiquetado_conecta4
+
 cant_imgs=1
 nombres_imagenes=nombres_img(cant_imgs)
 print(nombres_imagenes)
+
 
 for img in range(cant_imgs):
     #Formar la ruta de la imagen
@@ -18,19 +25,33 @@ for img in range(cant_imgs):
 
     #Invertir imagen
     imagen=inv_img(img_otsu)
-    plt.imshow(imagen)
+
+
+    primera_pasada_conecta8 = primer_etiquetado_conecta8(imagen[:,:,0])
+
+    segunda_pasada_conecta8 = segundo_etiquetado_conecta8(imagen[:,:,0])
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(primera_pasada_conecta8)
+    plt.title('Imagen 1')
+
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(segunda_pasada_conecta8)
+    plt.title('Imagen 2')
+
+    plt.tight_layout()
     plt.show()
 
-# Ejemplo de uso
-n = 6  # Número de elementos
-uf = UF(n)
+'''
 
-# Unimos algunos conjuntos
-uf.union(0, 1)
-uf.union(2, 3)
-uf.union(4, 5)
+binarizada_prueba = otsu('prueba.jpg')
 
-# Verificamos la pertenencia a los conjuntos
-print(uf.find(0))  # Debería devolver 0, ya que 0 y 1 están en el mismo conjunto
-print(uf.find(3))  # Debería devolver 2, ya que 2 y 3 están en el mismo conjunto
-print(uf.find(5))  # Debería devolver 4, ya que 4 y 5 están en el mismo conjunto
+etiquetada_prueba = EtiquetadoComponentes(binarizada_prueba[:,:,0])
+
+plt.imshow(etiquetada_prueba)
+plt.show()
+
+'''
